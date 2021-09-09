71eab97d660e53fc73bfd791b1e64d768be5d9d2
CIKiller <65186449+CIKiller@users.noreply.github.com>
2021-09-08 20:49:12 -0600

    Update fahrenheit2celsiusHW.c

--- C_Examples/Chapter1/fahrenheit2celsiusHW.c
+++ C_Examples/Chapter1/fahrenheit2celsiusHW.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
 float convert(float fahr) {
-  printf("Function call", "\n");
-	
-  printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
-	
-  printf("\n");
-  return (5.0 / 9.0) * (fahr - 32);
+  return (5.0 / 9.0) * (fahr - 32.0);
   //This is the most simple way to convert fahrenheit in a function.
 }
 
 void main(){
 	float fahr, celsius;
 	
-	for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+	for (fahr = 0; fahr <= 300.0; fahr = fahr + 20.0) {
 		celsius = convert(fahr);
 		//Call of the function.
 		

d821b3c7919bfc8c8cfad134f55b3c148afafc64
CIKiller <65186449+CIKiller@users.noreply.github.com>
2021-09-08 20:57:44 -0600

    Update fahrenheit2celsius-for.c

--- C_Examples/Chapter1/fahrenheit2celsius-for.c
+++ C_Examples/Chapter1/fahrenheit2celsius-for.c
@@ -7,8 +7,14 @@ float convert(float fahr) {
 
 main()
 {
-	  float fahr;
+	  int fahr;
+	
+	  float celsius;
 
-	  for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-		    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+	  for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+		  celsius = convert((float)fahr);
+		  
+		  printf("%3d %6.1f\n", fahr, celsius);
+		  
+	  }
 }

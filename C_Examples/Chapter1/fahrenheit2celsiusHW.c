6d46e09f9d9235679dd84dd00eb8d0e4fd275328
CIKiller <65186449+CIKiller@users.noreply.github.com>
2021-09-08 20:31:01 -0600

    Update fahrenheit2celsiusHW.c

--- C_Examples/Chapter1/fahrenheit2celsiusHW.c
+++ C_Examples/Chapter1/fahrenheit2celsiusHW.c
@@ -3,6 +3,8 @@
 float convert(float fahr) {
   float celsius = ((fahr - 32.0) * 0.55);
   
+  print("Celsius:", celsius);
+
   return celsius;
 }
 
@@ -10,8 +12,7 @@ void main() {
 	  float fahr, celsius;
 
 	  for (fahr = 0; fahr <= 300.00; fahr = fahr + 20.00) {
+          print("fahr:", fahr);
 		    celsius = convert(fahr);
-		  
-		    printf("%d\t%d\n", fahr, celsius);
 	  }
 }

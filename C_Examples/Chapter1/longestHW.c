e8390e0ac7fea02a1cc3c2b7f51673ce613a45c7
CIKiller <65186449+CIKiller@users.noreply.github.com>
2021-09-08 18:25:13 -0600

    Update longestHW.c

--- C_Examples/Chapter1/longestHW.c
+++ C_Examples/Chapter1/longestHW.c
@@ -31,9 +31,10 @@ int getaline(char s[], int lim)
     int c, i;
 
     for (i = 0; (c=getchar())!=EOF && c!='\n'; ++i)
-	s[i] = c;
-    if (c == '\n') {
-	s[i] = c;
+	if i < lim - 1
+		s[i] = c;
+	    if (c == '\n') {
+		s[i] = c;
 	++i;
     }
     s[i] = '\0';

7328a6d90eb64f0f4e1934b719c9e81b129d8c6d
CIKiller <65186449+CIKiller@users.noreply.github.com>
2021-09-16 11:37:58 -0600

    Update Lab1.c

--- Labs/Lab1.c
+++ Labs/Lab1.c
@@ -55,7 +55,7 @@ int count_tokens(char* str){
 	
 	int tokens = 0;
 	
-	for (int i = 0; i < sizeof(str); i++) {
+	for (int i = 0; i != '\0' ; i++) {
 		printf("%c", str[i]);
 		if (delim_character(str[i])) {
 			tokens++;

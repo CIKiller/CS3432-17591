63175f2f518ad88e556ae87c6170c6907fb94ed2
CIKiller <65186449+CIKiller@users.noreply.github.com>
2021-09-19 14:56:08 -0600

    Update Lab1.c

--- Labs/Lab1.c
+++ Labs/Lab1.c
@@ -110,6 +110,10 @@ char** tokenize(char* str){
 			
 			int k = j;
 			
+			for (k; str[k] != ' '; k++) {
+				printf("str[k]: %c \n", str[k]);
+			}
+			
 			printf("str[k]: %c \n", str[k]);
 			
 			i++;
@@ -125,18 +129,6 @@ char** tokenize(char* str){
 
 
 void print_all_tokens(char** tokens){
-	
-}
-
-void delay(int number_of_seconds) {
-    // Converting time into milli_seconds
-    int milli_seconds = 1000 * number_of_seconds;
-  
-    // Storing start time
-    clock_t start_time = clock();
-  
-    // looping till required time is not achieved
-    while (clock() < start_time + milli_seconds);
 	
 }
 

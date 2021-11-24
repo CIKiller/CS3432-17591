abead4f4e1bbc8abd1f480834d6b1d8b80dc1e3c
CIKiller <65186449+CIKiller@users.noreply.github.com>
2021-11-24 14:20:29 -0700

    Add files via upload

---
+++ Homework2/map_list.s
@@ -0,0 +1,84 @@
+.text
+main:   
+    jal x1, create_default_list
+    addi x18, x10, 0  # x18 = x10 = address of head of list
+
+    #print the list
+    jal x1, print_list
+
+    # print a newline
+    jal x1, print_newline
+
+    # load args to map
+    addi x10, x18, 0  # load the address of the first node
+
+    # load the address of the function to be called
+    la  x11, square
+
+    # issue the call to map
+    jal x1, map
+
+    #print the list
+    addi x10, x18, 0
+    jal x1, print_list
+
+    addi a0, x0, 10
+    ecall #Terminate the program
+
+map:
+#### YOUR CODE GOES HERE ####
+
+
+
+
+square:
+    mul x10 ,x10, x10
+    jalr x0, 0(x1)
+
+create_default_list:
+    addi sp, sp, -16
+    sw  ra, 0(sp)
+    sw  s0, 4(sp)
+    sw  s1, 8(sp)
+    li  s0, 0       # pointer to the last node we handled
+    li  s1, 0       # number of nodes handled
+loop:   #do...
+    li  a0, 8
+    jal ra, malloc      # get memory for the next node
+    sw  s1, 0(a0)   # node->value = i
+    sw  s0, 4(a0)   # node->next = last
+    add s0, a0, x0  # last = node
+    addi    s1, s1, 1   # i++
+    addi t0, x0, 10
+    bne s1, t0, loop    # ... while i!= 10
+    lw  ra, 0(sp)
+    lw  s0, 4(sp)
+    lw  s1, 4(sp)
+    addi sp, sp, 16
+    jr ra
+
+print_list:
+    bne a0, x0, printMeAndRecurse
+    jr ra       # nothing to print
+printMeAndRecurse:
+    add t0, a0, x0  # t0 gets current node address
+    lw  a1, 0(t0)   # a1 gets value in current node
+    addi a0, x0, 1      # prepare for print integer ecall
+    ecall
+    addi    a1, x0, ' '     # a0 gets address of string containing space
+    addi    a0, x0, 11      # prepare for print string syscall
+    ecall
+    lw  a0, 4(t0)   # a0 gets address of next node
+    jal x0, print_list  # recurse. We don't have to use jal because we already have where we want to return to in ra
+
+print_newline:
+    addi    a1, x0, '\n' # Load in ascii code for newline
+    addi    a0, x0, 11
+    ecall
+    jr  ra
+
+malloc:
+    addi    a1, a0, 0
+    addi    a0, x0 9
+    ecall
+    jr  ra
